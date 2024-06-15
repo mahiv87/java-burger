61c2b13262a893d0b01a83ca515b3dd7f2c8c379
mahiv87 <maherreraiv1987@gmail.com>
2024-06-15 17:40:18 -0600

    GET resquest

--- src/main/java/com/example/menu/item/ItemController.java
+++ src/main/java/com/example/menu/item/ItemController.java
@@ -1,9 +1,32 @@
 package com.example.menu.item;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/menu/items")
 public class ItemController {
+    private final ItemService service;
+
+    public ItemController(ItemService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> findAll() {
+        List<Item> items = service.findAll();
+        return ResponseEntity.ok().body(items);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> find(@PathVariable("id") Long id) {
+        Optional<Item> item = service.find(id);
+        return ResponseEntity.of(item);
+    }
 }
