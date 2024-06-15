74d568f51c235cac42a38042dd28cbbae483db6c
mahiv87 <maherreraiv1987@gmail.com>
2024-06-15 17:52:12 -0600

    PUT route for item

--- src/main/java/com/example/menu/item/ItemController.java
+++ src/main/java/com/example/menu/item/ItemController.java
@@ -35,4 +35,19 @@ public class ItemController {
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(created.getId()).toUri();
         return ResponseEntity.created(location).body(created);
     }
+
+    public ResponseEntity<Item> update(@PathVariable("id") Long id, @RequestBody Item updatedItem) {
+        Optional<Item> updated = service.update(id, updatedItem);
+
+        return updated
+                .map(value -> ResponseEntity.ok().body(value))
+                .orElseGet(() -> {
+                    Item created = service.create(updatedItem);
+                    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(created.getId())
+                            .toUri();
+                    return ResponseEntity.created(location).body(created);
+                });
+    }
 }
