0356a8fea2bfd02c2866df1a35fab1286f924ac5
mahiv87 <maherreraiv1987@gmail.com>
2024-06-16 08:28:46 -0600

    SecurityConfig class done for now

--- src/main/java/com/example/menu/security/SecurityConfig.java
+++ src/main/java/com/example/menu/security/SecurityConfig.java
@@ -1,4 +1,25 @@
 package com.example.menu.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/menu/items/**").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .cors(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
+                )
+                .build();
+
+    }
 }
